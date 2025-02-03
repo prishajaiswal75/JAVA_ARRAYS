457357e836499ed931817e9c9bae6ae9a2343013
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-02-03 16:59:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Main.java
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = UserInput.getNumbers();
+        
+        // Task 1: Separate even and odd numbers
+        ArrayLists.separateEvenOdd(numbers);
+        
