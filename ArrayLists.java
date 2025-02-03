dd22b93c0b5520fe94f01f6e3e437a07d589c22e
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-02-03 17:07:05 +0530

    Separating even and odd
    as well as printing them

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,3 +1,20 @@
 // ArrayLists.java
 
 import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void separateEvenOdd(int[] numbers) {
+        ArrayList<Integer> even = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
+        
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                even.add(num);
+            } else {
+                odd.add(num);
+            }
+        }
+        
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
