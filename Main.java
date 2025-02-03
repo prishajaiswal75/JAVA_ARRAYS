9d48cf0a2934835918fbd79a520095d6fcc51841
prishajaiswal75 <164599457+prishajaiswal75@users.noreply.github.com>
2025-02-03 17:01:20 +0530

    Task 2 smallest distance

--- Main.java
+++ Main.java
@@ -9,3 +9,5 @@ public class Main {
         // Task 1: Separate even and odd numbers
         ArrayLists.separateEvenOdd(numbers);
         
+        // Task 2: Find smallest distance between neighboring numbers
+        System.out.println("Index of first number with smallest distance: " + ArrayLists.findSmallestDistance(numbers));
